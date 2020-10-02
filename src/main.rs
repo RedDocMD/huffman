c38467694d908e78656ab28fadec5cac323ac46a
Deep Majumder <deep.majumder2019@gmail.com>
2020-10-02 23:11:19 +0530

    Encoder ready

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,45 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn encode_file(inp_filename: &str, out_filename: &str, code: &huffman::HuffmanCode) -> Result<(), Box<dyn Error>> {
+fn encode_file(
+    inp_filename: &str,
+    out_filename: &str,
+    code: &huffman::HuffmanCode,
+) -> Result<(), Box<dyn Error>> {
     let input_file = File::open(inp_filename)?;
     let mut output_file = File::create(out_filename)?;
     let mut buf_reader = BufReader::new(input_file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
-    let mut buf = Vec::new();
+
+    let mut bin_digits = Vec::new();
     for character in contents.chars() {
         if let Some(vec) = &mut code.encode_char(character) {
-            buf.append(vec);
+            bin_digits.append(vec);
         }
     }
+
+    let mut buf: Vec<u8> = vec![0; 10];
+    let mut buf_idx = 0;
+    let mut bin_idx = 0;
+    let mut byte: u8 = 0;
+    while bin_idx < bin_digits.len() {
+        if bin_idx > 0 && bin_idx % 8 == 0 {
+            buf[buf_idx] = byte;
+            byte = 0;
+            buf_idx += 1;
+        }
+        if buf_idx == 10 {
+            output_file.write(&buf)?;
+            buf_idx = 0;
+            for i in 0..10 {
+                buf[i] = 0;
+            }
+        }
+        byte = (byte << 1) + bin_digits[bin_idx];
+        bin_idx += 1;
+    }
+    output_file.write(&buf)?;
     // let lines = contents.split('\n');
     // for line in lines {
     //     let line_string = String::from(line);
